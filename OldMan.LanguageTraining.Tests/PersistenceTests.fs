87a625efe059c3f6db03cc3bb1449e097c597953
max <7413294+ModernRonin@users.noreply.github.com>
2019-05-18 22:50:53 +0100

    2nd test :-)

--- OldMan.LanguageTraining.Tests/PersistenceTests.fs
+++ OldMan.LanguageTraining.Tests/PersistenceTests.fs
@@ -5,15 +5,28 @@ open FsUnitTyped
 open OldMan.LanguageTraining.Persistence
 open OldMan.LanguageTraining.Domain
     
+let shouldNotHappen() = raise (AssertionException("Should not get here"))
+
+type UnderTest= OldMan.LanguageTraining.Persistence.Persistence
+
 module ``GetConfiguration``=
-    type UnderTest= OldMan.LanguageTraining.Persistence.Persistence
     [<Test>]
     let ``loads from CSV``() =
         let loader kind= match kind with
             | Configuration -> "English,German"
-            | _ -> raise (AssertionException("loads wrong data type"))
-        let saver kind data = raise (AssertionException("tries to save"))
+            | _ -> shouldNotHappen()
+        let saver _ _ = shouldNotHappen()
 
         let underTest= new UnderTest(loader, saver)
         underTest.GetConfiguration() |> shouldEqual { LeftLanguageName="English"; RightLanguageName="German"}
         
+module ``UpdateConfiguration``=
+    [<Test>]
+    let ``saves to CSV``()=
+        let loader _ = shouldNotHappen()
+        let saver kind data = match kind with
+            | Configuration -> data |> shouldEqual "English,German\r\n"
+            | _ -> shouldNotHappen()
+
+        let underTest= new UnderTest(loader, saver)
+        underTest.UpdateConfiguration { LeftLanguageName="English"; RightLanguageName="German"} 
