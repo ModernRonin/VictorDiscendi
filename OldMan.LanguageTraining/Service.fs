d651ce1f9c72e4528544dc4c04c7d5262a2e523d
max <7413294+ModernRonin@users.noreply.github.com>
2019-05-19 17:44:52 +0100

    thoughts for next step

---
+++ OldMan.LanguageTraining/Service.fs
@@ -0,0 +1,18 @@
+﻿module OldMan.LanguageTraining.Service
+
+open System
+open OldMan.LanguageTraining.Domain
+open OldMan.LanguageTraining.Persistence
+
+(*
+add word pair (left, right, tags)
+edit word pair (only left, right, tags) - old, nu
+list word pairs (all data)
+list tags (all data)
+list word pairs for tags (only those matching)
+update language names
+get language names
+get question for quizSettings
+score question for question and result
+
+*)
