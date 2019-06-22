9a0c579d09b96f9d03cb62f29b8c33480852feb9
max <7413294+ModernRonin@users.noreply.github.com>
2019-06-22 12:27:18 +0100

    builds with websharper

--- OldMan.LanguageTraining/Id.fs
+++ OldMan.LanguageTraining/Id.fs
@@ -1,18 +1,22 @@
 ﻿namespace OldMan.LanguageTraining.Domain
 
+open WebSharper
 type Id = Id of int64 
 
 module Id=
     let unwrap (Id i)= i
     let uninitialized= Id (int64 0)
     let wrap (what: int64)= Id (int64 what)
+    [<Inline("$x.Id")>]
     let inline fromRaw (x: ^R)=
        let i64 =(^R: (member Id: int64) (x))
        Id i64
 
+    [<Inline("$x.Id")>]
     let inline from (x: ^R)=
        (^R: (member Id: Id) (x))
         
+    //[<Inline("Math.max(...$rows.map(e => e.Id))")>]
     let inline nextAfter (rows: ^record seq)= 
         let frozen= rows |> Seq.map fromRaw |> Array.ofSeq
 
