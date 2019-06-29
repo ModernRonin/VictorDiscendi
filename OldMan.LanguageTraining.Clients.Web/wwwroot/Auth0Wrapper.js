bef4144b131d3ca3bbada90daf9533fd6de223c8
max <7413294+ModernRonin@users.noreply.github.com>
2019-06-29 23:03:13 +0100

    added more JS

--- OldMan.LanguageTraining.Clients.Web/wwwroot/Auth0Wrapper.js
+++ OldMan.LanguageTraining.Clients.Web/wwwroot/Auth0Wrapper.js
@@ -1,4 +1,30 @@
-﻿var Auth0Wrapper = {}
-Auth0Wrapper.doLogin = function() {
-    return "blabla";
-};
+﻿var Auth0Wrapper =
+{
+     _config: {
+        domain: "oldman.eu.auth0.com",
+        clientId: "PCXHj3vHt1gCjgVkdYwRuUHmQtt8s11v"
+    },
+
+    _auth0: null,
+
+    configureClient : async () => {
+
+        Auth0Wrapper._auth0 = await createAuth0Client({
+                    domain: Auth0Wrapper._config.domain,
+                    client_id: Auth0Wrapper._config.clientId
+                });
+    }, 
+    updateAuthenticationStateus: async () => {
+        Auth0Wrapper.isAuthenticated = await Auth0Wrapper._auth0.isAuthenticated();
+    },
+    isAuthenticated: false,
+
+    doLogin: function() {
+        return "blabla";
+    }   
+}
+window.onload = async () => {
+    await Auth0Wrapper.configureClient();
+    await Auth0Wrapper.updateAuthenticationStateus();
+}
+
