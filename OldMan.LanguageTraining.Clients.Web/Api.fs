d23181a78631a808a218c6da9b9010a98549eaac
max <7413294+ModernRonin@users.noreply.github.com>
2019-06-29 15:07:00 +0100

    implemented LocalStoragePersistenceStore added seed() function

--- OldMan.LanguageTraining.Clients.Web/Api.fs
+++ OldMan.LanguageTraining.Clients.Web/Api.fs
@@ -4,99 +4,114 @@ open System
 open OldMan.LanguageTraining
 open OldMan.LanguageTraining.Domain
 
-type private Service()=
+open WebSharper.UI
+open WebSharper.UI.Storage
+
+type private LocalStoragePersistenceStore()=
+    let create name keyExtractor=
+        let serializer= (Serializer.Default<'T>)
+        let storage= (LocalStorage name serializer)
+        ListModel.CreateWithStorage keyExtractor storage
+
+    let configList= create "config-store" (fun (_: SerializableConfiguration) -> "default")
+    let tagsList= create "tags-store" (fun (t: SerializableTag) -> t.Id)
+    let pairsList= create "pairs-store" (fun (p: SerializablePair) -> p.Id)
+    let associationsList= create "associations-store" (fun (a: SerializableTagPairAssociation) -> a)
+
+    interface IPersistenceStore with
+        member this.loadConfig()=
+            match configList |> List.ofSeq with 
+            | [] -> {LeftName=""; RightName=""}
+            | head::_ -> head
+
+        member this.saveConfig(config)= configList.Set [config]
+
+        member this.loadTags()= tagsList |> List.ofSeq
+        member this.saveTags(tags)= tagsList.Set tags
+        member this.loadPairs()= pairsList |> List.ofSeq
+        member this.savePairs(pairs)= pairsList.Set pairs
+        member this.loadAssociations()= associationsList |> List.ofSeq
+        member this.saveAssociations(associations)= associationsList.Set associations
+            
+
+let private store= new LocalStoragePersistenceStore() :> IPersistenceStore
+let mutable private _service= (new Service(store)) :> IService
+
+let private doSeed()=
+    {LeftName="English"; RightName="Deutsch"} |> store.saveConfig
     let mutable nextId= 1L
     let makeId()=
         let result= nextId
         nextId <- result + 1L
-        result |> Id.wrap
-    let mutable tags= List.empty<Tag>
-    let makeTag text = 
-        match tags |> List.tryFind (fun t -> t.Text=text) with
-        | Some t -> t
-        | None ->
-            let nu=
-                {
-                    Text= text
-                    Id= makeId()
-                }
-            tags <- nu :: tags
-            nu
-    let pair english german tags= 
-        { WordPair.create (Word english, Word german, tags |> List.map makeTag) with Id= makeId() }
-    let mutable pairs= 
+        result
+    let makeTag text: SerializableTag= 
+        {
+            Id= makeId()
+            Text= text
+        }
+    [
+        "noun"
+        "fruit"
+        "verb"
+        "cooking"
+        "adjective"
+        "color"
+    ] |> List.map makeTag |> store.saveTags
+    let makePair (english, german)=
+        {
+            Id= makeId()
+            Left= english
+            Right= german
+            Created= "2019-03-07-07-30-00"
+            LastAsked= "2019-03-07-07-30-00"
+            TimesAsked= 0L
+            LeftScore= -2
+            RightScore= -2
+        }
+    let special= 
+        {
+            Id= makeId()
+            Left= "old"
+            Right= "alt"
+            Created= "1974-07-23-00-02-00"
+            LastAsked= "2019-03-07-07-30-00"
+            TimesAsked= 13L
+            LeftScore= 7
+            RightScore= 5
+        }
+    let others=
         [
-            pair "apple" "Apfel" ["noun"; "fruit"]
-            pair "orange" "Orange" ["noun"; "fruit"]
-            pair "peel" "schälen" ["verb"; "cooking"]
-            pair "red" "rot" ["adjective"; "color"]
-            {
-                pair "old" "alt" ["adjective"] 
-                with 
-                    Created= new DateTime(1974, 7, 23, 0, 2, 0)
-                    ScoreCard= 
-                    {
-                        LastAsked= new DateTime(2018, 02, 03, 16, 30, 0)
-                        TimesAsked= Count 13u
-                        LeftScore= Score 7
-                        RightScore= Score 5
-                    }
-            }
-        ]
-    interface IService with
-        member this.UpdateLanguageNames(configuration)= ()
+            ("apple", "Apfel")
+            ("orange", "Orange")
+            ("peel", "schälen")
+            ("red", "rot")
+        ] |> List.map makePair
+    special::others |> store.savePairs
+    let makeAssociation english tagText=
+        {
+            PairId= store.loadPairs() 
+                        |> List.filter (fun p -> p.Left=english) 
+                        |> List.map (fun p -> p.Id) 
+                        |> List.head
+            TagId= store.loadTags()
+                        |> List.filter (fun t -> t.Text=tagText)
+                        |> List.map (fun t -> t.Id)
+                        |> List.head
+        }
+    let makeAssociations (english, tags)=
+        tags |> List.map (makeAssociation english)
+    [
+        ("apple", ["noun"; "fruit"])
+        ("orange", ["noun"; "fruit"])
+        ("peel", ["verb"; "cooking"])
+        ("red", ["adjective"; "color"])
+        ("old", ["adjective"])
+    ] |> List.collect makeAssociations |> store.saveAssociations
 
-        member this.GetLanguageNames()= {LeftLanguageName="English"; RightLanguageName="German"}
-
-        member this.ListWordPairs()= pairs
-
-        member this.ListWordPairsForTags condition= 
-            pairs |> TagCondition.filter condition
-
-        member this.AddWordPair(definition)=
-            let result= definition |> WordPair.create
-            pairs <- result :: pairs
-            result
-
-        member this.UpdateWordPair (id,definition)=
-            let withoutOld= pairs |> List.filter (fun p -> p.Id<>id)
-            let updated= {(definition |> WordPair.create) with Id=id}
-            pairs <- updated :: withoutOld
-
-        member this.ListTags()= 
-            let count tag= pairs |> List.filter (fun p -> p.Tags |> List.contains tag) |> List.length
-            pairs |> List.collect(fun p -> p.Tags) |> List.distinct |> List.map (fun t -> (t, (count t)))
-
-        member this.AddOrUpdateTag(tag)=
-            tag
-
-        member this.GenerateQuestion(settings)= 
-            match settings.Type with
-                | FreeEntry -> 
-                    (
-                        {PairId = Id.wrap 1L; Side=Right}, 
-                        FreeEntryQuestion { Prompt= "Apple"; CorrectAnswer="Apfel"}
-                    )
-                | MultipleChoice _ -> 
-                    (
-                        {PairId = Id.wrap 2L; Side=Right}, 
-                        MultipleChoiceQuestion {
-                                                  Prompt="Orange"
-                                                  CorrectAnswer= "orange"
-                                                  Choices= ["orange"; "apple"; "peel"]
-                                               }
-                    )                    
-
-        member this.ScoreQuestionResult (({PairId= pairId}, _, result) :WordReference*Question*QuestionResult)=
-            let target= pairs |> List.find (fun p -> p.Id=pairId)
-            let delta= match result with 
-                        | Correct -> 1
-                        | Incorrect -> -1
-            let withoutOld= pairs |> List.filter (fun p -> p.Id<>pairId)
-            let updated= {target with ScoreCard= {target.ScoreCard with RightScore=Score.add target.ScoreCard.RightScore delta}}
-            pairs <- updated :: withoutOld
+let seed()=
+    match store.loadConfig() with 
+    | {LeftName=""; RightName=""} ->
+        doSeed()
+    | _ -> ()
     
-
-let mutable private _service= (new Service()) :> IService
-
 let service()= _service
