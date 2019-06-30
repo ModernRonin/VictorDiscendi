a01d43c631cf3b005ccf959684a11aaaf1aaaca7
max <7413294+ModernRonin@users.noreply.github.com>
2019-06-30 12:30:46 +0100

    wrapper module for the Auth0 JS code

---
+++ OldMan.LanguageTraining.Clients.Web/Auth0.fs
@@ -0,0 +1,11 @@
+﻿module OldMan.LanguageTraining.Web.Auth0
+
+open WebSharper
+open WebSharper.JavaScript
+
+[<Direct("window.Auth0Wrapper.login()")>]
+let login ()= X<Promise<unit>>
+
+[<Direct("window.Auth0Wrapper.isAuthenticated")>]
+let isLoggedIn()= X<bool>
+
